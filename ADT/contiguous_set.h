b4aea735682ff6de7094c19730a29dd8db2df308
Jared Schmitz <jared@cs.columbia.edu>
2014-02-09 19:40:08 -0500

    Work on accessors and search functions

--- ADT/contiguous_set.h
+++ ADT/contiguous_set.h
@@ -1,13 +1,20 @@
 #ifndef LIBCPP_CONTIGUOUS_SET_H
 #define LIBCPP_CONTIGUOUS_SET_H
 
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <memory>
 #include <vector>
 
 template <class Key, class Compare = std::less<Key>>
 class contiguous_set {
 private:
 	std::vector<Key> storage;
-	typedef std::allocator_traits<allocator_type> AllocatorTraits;
+	bool equivalent(const Key& k1, const Key& k2) {
+		return !key_comp()(k1, k2) && !key_comp()(k2, k1);
+	}
 public:
 	typedef Key key_type;
 	typedef Key value_type;
@@ -15,21 +22,23 @@ public:
 	typedef std::ptrdiff_t difference_type;
 	typedef Compare key_compare;
 	typedef Compare value_compare;
-	typedef decltype(storage)::allocator_type allocator_type;
+	typedef typename decltype(storage)::allocator_type allocator_type;
 	typedef value_type& reference_type;
 	typedef const value_type& const_reference_type;
-	typedef AllocatorTraits::pointer pointer;
-	typedef AllocatorTraits::const_pointer const_pointer;
-	typedef decltype(storage)::iterator iterator;
-	typedef decltype(storage)::const_iterator const_iterator;
+	typedef typename std::allocator_traits<allocator_type>::pointer pointer;
+	typedef typename std::allocator_traits<allocator_type>::const_pointer const_pointer;
+	typedef typename decltype(storage)::iterator iterator;
+	typedef typename decltype(storage)::const_iterator const_iterator;
 	typedef std::reverse_iterator<iterator> reverse_iterator;
 	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
 	// Constructors, assignment, destructor
+	// TODO: All the ctors
 	contiguous_set& operator=(const contiguous_set&) = default;
 	contiguous_set& operator=(contiguous_set&&) = default;
-	contiguous_set& operator=(std::initializer_list<value_type> ilist)
-		: storage(ilist) {}
+	contiguous_set& operator=(std::initializer_list<value_type> ilist) {
+		storage.assign(ilist);
+	}
 
 	~contiguous_set() = default;
 
@@ -55,31 +64,65 @@ public:
 	size_type size() const { return storage.size(); }
 	size_type max_size() const { return storage.max_size(); }
 
-	key_compare key_comp() const { return Key(); }
+	key_compare key_comp() const { return Compare(); }
 	value_compare value_comp() const { return Compare(); }
 
 	// Modifiers
 	void clear() { storage.clear(); }
-	size_type erase(const key_type& key);
+	size_type erase(const key_type& key) {
+		auto I = find(key);
+		if (I != end())
+			storage.erase(I);
+	}
 
-	void swap(contiguous_set& other);
+	void swap(contiguous_set& other) {
+		storage.swap(other.storage);
+	}
 
-	// TODO: insert, emplace, emplace_hint
+	// insert, emplace, emplace_hint
 	std::pair<iterator, bool> insert(const value_type& value);
 	std::pair<iterator, bool> insert(value_type&& value);
 
 	iterator insert(const_iterator hint, const value_type& value);
 	iterator insert(const_iterator hint, value_type&& value);
+
 	template <class InputIt>
-	void insert(InputIt first, InputIt last);
+	void insert(InputIt first, InputIt last) {
+		// Procedure is to insert the new elements at the end, sort
+		// them, then do an inplace merge of the entire vector.
+		size_type old_size = size();
+		storage.insert(end(), first, last);
+		std::sort(begin() + old_size, end(), Compare());
+		std::inplace_merge(begin(), begin() + old_size, end(),
+				Compare());
+	}
 
-	void insert(std::initializer_list<value_type> ilist);
+	void insert(std::initializer_list<value_type> ilist) {
+		insert(ilist.begin(), ilist.end());
+	}
 
 	template <class... Args>
-	std::pair<iterator, bool> emplace(Args&&... args);
+	std::pair<iterator, bool> emplace(Args&&... args) {
+		// Handle the annoying case where the type may not be moveable.
+		value_type tmp(std::forward<Args>(args)...);
+		auto I = lower_bound(tmp);
+		if (I == end()) {
+			storage.emplace(I, std::forward<Args>(args)...);
+			return std::make_pair(I, true);
+		} else if (equivalent(*I, tmp)) {
+			return std::make_pair(I, false);
+		} else {
+			storage.emplace(I, std::forward<Args>(args)...);
+			return std::make_pair(I, true);
+		}
+	}
 
 	template <class... Args>
-	iterator emplace_hint(const_iterator hint, Args&&... args);
+	iterator emplace_hint(const_iterator, Args&&... args) {
+		// XXX: We can't use the hint efficiently because of the
+		// contiguous nature of the storage. :)
+		return emplace(std::forward<Args>(args)...).first;
+	}
 
 	// Search operations we need to implement
 	size_type count(const key_type& key) const {
